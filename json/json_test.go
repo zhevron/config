4d81cc8238903212942191fe77046a05c239cc87
Thomas Lokshall <thomas@lokshall.no>
2015-02-17 14:18:09 +0100

    Updated JSON package to new API

--- json/json_test.go
+++ json/json_test.go
@@ -13,200 +13,193 @@ func Test(t *testing.T) {
 	TestingT(t)
 }
 
-type GocfgJSONSuite struct {
-	cfg *Configuration
-	yml string
-	f   *os.File
+type JSONSuite struct {
+	data string
+	file *os.File
 }
 
-var _ = Suite(&GocfgJSONSuite{})
+var _ = Suite(&JSONSuite{})
 
-func (s *GocfgJSONSuite) SetUpTest(c *C) {
-	s.cfg = NewConfiguration()
-	s.f, _ = ioutil.TempFile("", "gocfg")
-	s.f.Close()
-	s.cfg.values["num"] = 12345
-	s.cfg.values["f"] = 1.2345
-	s.cfg.values["str"] = "some string"
-	s.cfg.values["b"] = true
-	s.cfg.values["nested"] = make(stringMap)
-	s.cfg.values["nested"].(stringMap)["value"] = 1
+func (s *JSONSuite) SetUpTest(c *C) {
+	s.file, _ = ioutil.TempFile("", "gocfg")
+	s.file.Close()
+	vars["num"] = 12345
+	vars["f"] = 1.2345
+	vars["str"] = "some string"
+	vars["b"] = true
+	vars["nested"] = make(stringMap)
+	vars["nested"].(stringMap)["value"] = 1
 }
 
-func (s *GocfgJSONSuite) TearDownTest(c *C) {
-	os.Remove(s.f.Name())
+func (s *JSONSuite) TearDownTest(c *C) {
+	os.Remove(s.file.Name())
 }
 
-func (s *GocfgJSONSuite) TestNewConfiguration(c *C) {
-	cfg := NewConfiguration()
-	c.Check(cfg, Not(Equals), nil)
-}
-
-func (s *GocfgJSONSuite) TestConfigurationLoad(c *C) {
+func (s *JSONSuite) TestLoad(c *C) {
 	json := `{"test": "value"}`
-	err := s.cfg.Load(json)
-	_, found := s.cfg.values["test"]
+	err := Load(json)
+	_, found := vars["test"]
 	c.Check(err, Equals, nil)
 	c.Check(found, Equals, true)
 	json = `{illegaljson, "test": "value"}`
-	err = s.cfg.Load(json)
+	err = Load(json)
 	c.Check(err, Not(Equals), nil)
 }
 
-func (s *GocfgJSONSuite) TestConfigurationSave(c *C) {
-	json, err := s.cfg.Save()
+func (s *JSONSuite) TestSave(c *C) {
+	json, err := Save()
 	c.Check(err, Equals, nil)
 	c.Check(len(json), Not(Equals), 0)
 }
 
-func (s *GocfgJSONSuite) TestConfigurationLoadFile(c *C) {
+func (s *JSONSuite) TestLoadFile(c *C) {
 	json := `{"test": "value", "other": {"nested": 0}}`
-	f, _ := os.Create(s.f.Name())
+	f, _ := os.Create(s.file.Name())
 	f.WriteString(json)
 	f.Close()
-	err := s.cfg.LoadFile(s.f.Name())
-	_, found := s.cfg.values["test"]
+	err := LoadFile(s.file.Name())
+	_, found := vars["test"]
 	c.Check(err, Equals, nil)
 	c.Check(found, Equals, true)
-	err = s.cfg.LoadFile("nonexistant.file")
+	err = LoadFile("nonexistant.file")
 	c.Check(err, Not(Equals), nil)
 }
 
-func (s *GocfgJSONSuite) TestConfigurationSaveFile(c *C) {
-	err := s.cfg.SaveFile(s.f.Name())
-	fi, _ := os.Stat(s.f.Name())
+func (s *JSONSuite) TestSaveFile(c *C) {
+	err := SaveFile(s.file.Name())
+	fi, _ := os.Stat(s.file.Name())
 	c.Check(err, Equals, nil)
 	c.Check(fi.Size(), Not(Equals), 0)
 }
 
-func (s *GocfgJSONSuite) TestConfigurationSet(c *C) {
-	s.cfg.Set("test", "value")
-	v, found := s.cfg.values["test"]
+func (s *JSONSuite) TestSet(c *C) {
+	Set("test", "value")
+	v, found := vars["test"]
 	c.Check(found, Equals, true)
 	c.Check(v, Equals, "value")
 }
 
-func (s *GocfgJSONSuite) TestConfigurationRemove(c *C) {
-	s.cfg.values["test"] = "value"
-	s.cfg.Remove("test")
-	_, found := s.cfg.values["test"]
+func (s *JSONSuite) TestRemove(c *C) {
+	vars["test"] = "value"
+	Remove("test")
+	_, found := vars["test"]
 	c.Check(found, Equals, false)
 }
 
-func (s *GocfgJSONSuite) TestConfigurationGet(c *C) {
-	s.cfg.values["test"] = "value"
-	v := s.cfg.Get("test", "default")
+func (s *JSONSuite) TestGet(c *C) {
+	vars["test"] = "value"
+	v := Get("test", "default")
 	c.Check(v, Equals, "value")
-	v = s.cfg.Get("nested.value", "default")
+	v = Get("nested.value", "default")
 	c.Check(v, Not(Equals), "default")
-	v = s.cfg.Get("nonexistant", "default")
+	v = Get("nonexistant", "default")
 	c.Check(v, Equals, "default")
 }
 
-func (s *GocfgJSONSuite) TestConfigurationGetInt(c *C) {
-	v := s.cfg.GetInt("num", 0)
+func (s *JSONSuite) TestGetInt(c *C) {
+	v := GetInt("num", 0)
 	c.Check(reflect.TypeOf(v).Kind(), Equals, reflect.Int)
 	c.Check(v, Not(Equals), 0)
 }
 
-func (s *GocfgJSONSuite) TestConfigurationGetInt8(c *C) {
-	v := s.cfg.GetInt8("num", 0)
+func (s *JSONSuite) TestGetInt8(c *C) {
+	v := GetInt8("num", 0)
 	c.Check(reflect.TypeOf(v).Kind(), Equals, reflect.Int8)
 	c.Check(v, Not(Equals), 0)
 }
 
-func (s *GocfgJSONSuite) TestConfigurationGetInt16(c *C) {
-	v := s.cfg.GetInt16("num", 0)
+func (s *JSONSuite) TestGetInt16(c *C) {
+	v := GetInt16("num", 0)
 	c.Check(reflect.TypeOf(v).Kind(), Equals, reflect.Int16)
 	c.Check(v, Not(Equals), 0)
 }
 
-func (s *GocfgJSONSuite) TestConfigurationGetInt32(c *C) {
-	v := s.cfg.GetInt32("num", 0)
+func (s *JSONSuite) TestGetInt32(c *C) {
+	v := GetInt32("num", 0)
 	c.Check(reflect.TypeOf(v).Kind(), Equals, reflect.Int32)
 	c.Check(v, Not(Equals), 0)
 }
 
-func (s *GocfgJSONSuite) TestConfigurationGetInt64(c *C) {
-	v := s.cfg.GetInt64("num", 0)
+func (s *JSONSuite) TestGetInt64(c *C) {
+	v := GetInt64("num", 0)
 	c.Check(reflect.TypeOf(v).Kind(), Equals, reflect.Int64)
 	c.Check(v, Not(Equals), 0)
 }
 
-func (s *GocfgJSONSuite) TestConfigurationGetUint(c *C) {
-	v := s.cfg.GetUint("num", 0)
+func (s *JSONSuite) TestGetUint(c *C) {
+	v := GetUint("num", 0)
 	c.Check(reflect.TypeOf(v).Kind(), Equals, reflect.Uint)
 	c.Check(v, Not(Equals), 0)
 }
 
-func (s *GocfgJSONSuite) TestConfigurationGetUint8(c *C) {
-	v := s.cfg.GetUint8("num", 0)
+func (s *JSONSuite) TestGetUint8(c *C) {
+	v := GetUint8("num", 0)
 	c.Check(reflect.TypeOf(v).Kind(), Equals, reflect.Uint8)
 	c.Check(v, Not(Equals), 0)
 }
 
-func (s *GocfgJSONSuite) TestConfigurationGetUint16(c *C) {
-	v := s.cfg.GetUint16("num", 0)
+func (s *JSONSuite) TestGetUint16(c *C) {
+	v := GetUint16("num", 0)
 	c.Check(reflect.TypeOf(v).Kind(), Equals, reflect.Uint16)
 	c.Check(v, Not(Equals), 0)
 }
 
-func (s *GocfgJSONSuite) TestConfigurationGetUint32(c *C) {
-	v := s.cfg.GetUint32("num", 0)
+func (s *JSONSuite) TestGetUint32(c *C) {
+	v := GetUint32("num", 0)
 	c.Check(reflect.TypeOf(v).Kind(), Equals, reflect.Uint32)
 	c.Check(v, Not(Equals), 0)
 }
 
-func (s *GocfgJSONSuite) TestConfigurationGetUint64(c *C) {
-	v := s.cfg.GetUint64("num", 0)
+func (s *JSONSuite) TestGetUint64(c *C) {
+	v := GetUint64("num", 0)
 	c.Check(reflect.TypeOf(v).Kind(), Equals, reflect.Uint64)
 	c.Check(v, Not(Equals), 0)
 }
 
-func (s *GocfgJSONSuite) TestConfigurationGetFloat32(c *C) {
-	v := s.cfg.GetFloat32("f", 0.0)
+func (s *JSONSuite) TestGetFloat32(c *C) {
+	v := GetFloat32("f", 0.0)
 	c.Check(reflect.TypeOf(v).Kind(), Equals, reflect.Float32)
 	c.Check(v, Not(Equals), 0.0)
 }
 
-func (s *GocfgJSONSuite) TestConfigurationGetFloat64(c *C) {
-	v := s.cfg.GetFloat64("f", 0.0)
+func (s *JSONSuite) TestGetFloat64(c *C) {
+	v := GetFloat64("f", 0.0)
 	c.Check(reflect.TypeOf(v).Kind(), Equals, reflect.Float64)
 	c.Check(v, Not(Equals), 0.0)
 }
 
-func (s *GocfgJSONSuite) TestConfigurationGetBool(c *C) {
-	v := s.cfg.GetBool("b", false)
+func (s *JSONSuite) TestGetBool(c *C) {
+	v := GetBool("b", false)
 	c.Check(reflect.TypeOf(v).Kind(), Equals, reflect.Bool)
 	c.Check(v, Not(Equals), false)
 }
 
-func (s *GocfgJSONSuite) TestConfigurationGetString(c *C) {
-	v := s.cfg.GetString("str", "default")
+func (s *JSONSuite) TestGetString(c *C) {
+	v := GetString("str", "default")
 	c.Check(reflect.TypeOf(v).Kind(), Equals, reflect.String)
 	c.Check(v, Not(Equals), "default")
 }
 
-func (s *GocfgJSONSuite) BenchmarkConfigurationLoad(c *C) {
+func (s *JSONSuite) BenchmarkConfigurationLoad(c *C) {
 	for i := 0; i < c.N; i++ {
-		s.cfg.Load(s.yml)
+		Load(s.data)
 	}
 }
 
-func (s *GocfgJSONSuite) BenchmarkConfigurationSave(c *C) {
+func (s *JSONSuite) BenchmarkConfigurationSave(c *C) {
 	for i := 0; i < c.N; i++ {
-		s.cfg.Save()
+		Save()
 	}
 }
 
-func (s *GocfgJSONSuite) BenchmarkConfigurationGet(c *C) {
+func (s *JSONSuite) BenchmarkConfigurationGet(c *C) {
 	for i := 0; i < c.N; i++ {
-		s.cfg.Get("str", "default")
+		Get("str", "default")
 	}
 }
 
-func (s *GocfgJSONSuite) BenchmarkConfigurationGetNested(c *C) {
+func (s *JSONSuite) BenchmarkConfigurationGetNested(c *C) {
 	for i := 0; i < c.N; i++ {
-		s.cfg.Get("nested.value", "default")
+		Get("nested.value", "default")
 	}
 }
